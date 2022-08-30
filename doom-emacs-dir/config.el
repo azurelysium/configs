b70ed8b1562c973960b1f8cf86dc37ac6bae95a6
Minhwan Kim <azurelysium@gmail.com>
2022-08-30 12:52:33 +0900

    minor changes

--- doom-emacs-dir/config.el
+++ doom-emacs-dir/config.el
@@ -9,30 +9,7 @@
 (setq user-full-name "Minhwan Kim"
       user-mail-address "azurelysium@gmail.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-unicode-font' -- for unicode glyphs
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-vibrant)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -41,41 +18,6 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
-
-
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-
-;;(setq display-line-numbers-type 'nil)
 
 ;; Azurelysium's Settings
 (remove-hook 'doom-first-buffer-hook #'global-hl-line-mode)
@@ -88,16 +30,37 @@
 (set-terminal-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 
+;; bindings
+
 (global-set-key [left] 'windmove-left)          ; move to left window
 (global-set-key [right] 'windmove-right)        ; move to right window
 (global-set-key [up] 'windmove-up)              ; move to upper window
 (global-set-key [down] 'windmove-down)          ; move to lower window
 (global-set-key (kbd "C-c z") 'zoom-window-zoom)
 
+(:map evil-window-map
+  "<left>"     #'evil-window-left
+  "<right>"    #'evil-window-right
+  "<up>"       #'evil-window-up
+  "<down>"     #'evil-window-down)
+
+(map!
+ (:leader
+  (:prefix "f"
+   :desc "Toggle Treemacs" "t" #'+treemacs/toggle-project)
+  (:prefix "o"
+   :desc "Open kill ring" "k" #'+default/yank-pop
+   :desc "Open notes.org" "n" #'org-notes-open))
+
 ;; orgmode settings
+
 (add-hook 'org-mode-hook
   (lambda () (local-set-key (kbd "C-c C-b") 'org-mark-ring-goto)))
 (add-hook 'org-mode-hook (lambda () (display-line-numbers-mode -1)))
+
+;; programming
+
+(add-hook! prog-mode #'rainbow-delimiters-mode)
 
 (use-package! typescript-mode
   :init
