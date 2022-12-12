4076b286ff5bd356cf46c87b9e25fb6f0580e53b
Minhwan Kim <azurelysium@gmail.com>
2022-12-12 13:58:02 +0900

    minor changes

--- doom-emacs-dir/custom.el
+++ doom-emacs-dir/custom.el
@@ -5,11 +5,12 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("0d01e1e300fcafa34ba35d5cf0a21b3b23bc4053d388e352ae6a901994597ab1" default))
- '(package-selected-packages '(sass-mode go-mode)))
+ '(package-selected-packages
+   '(solidity-flycheck flymake-solidity solidity-mode sass-mode go-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(ts-fold-replacement-face ((t (:foreground nil :box nil :inherit font-lock-comment-face :weight light)))))
 (put 'narrow-to-region 'disabled nil)
