176947b2ec6068aff9efcf5d8aeb5c0d2343c809
Minhwan Kim <azurelysium@gmail.com>
2023-08-23 10:52:34 +0900

    changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -57,3 +57,15 @@ vim.api.nvim_set_keymap("n", "<leader>%", "<CMD>SearchReplaceSingleBufferOpen<CR
 
 -- show the effects of a search / replace in a live preview window
 vim.o.inccommand = "split"
+
+-- Tree-sitter
+require'nvim-treesitter.configs'.setup {
+  highlight = {
+    enable = true,
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
